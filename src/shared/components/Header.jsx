74277715a1713eb408ed1a7784c3e80614631b6c
Jesus Martin <jesus8.mb@gmail.com>
2025-09-22 09:39:55 +0200

    feat/ Header done, https://www.hyperui.dev/components/marketing/headers reference

--- src/shared/components/Header.jsx
+++ src/shared/components/Header.jsx
@@ -1,75 +1,112 @@
-import { Link } from 'react-router-dom';
-import Logo from '../assets/Logo.svg';
+import { Link } from "react-router-dom";
+import Logo from "../assets/Logo.svg";
+import { useState } from "react";
 
-// Referencia: https://www.hyperui.dev/components/marketing/headers
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-const Header = () => { 
+  return (
+    <header className="bg-green-light font-cardo py-3">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-20">
 
-  return(
-      <header className="bg-green-light font-cardo">
+          {/* Logo */}
+          <div className="flex items-center">
+            <Link to="/">
+              <span className="sr-only">Home</span>
+              <img src={Logo} alt="Logo" className="h-16 w-auto" />
+              
+            </Link>
+          </div>
 
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-          <div className="flex h-30 items-center justify-between">
-            
-            <div className="md:flex md:items-center md:gap-12">
-              <Link className="block text-teal-600" to="/">
-                <span className="sr-only">Home</span>
-                <img src={Logo} alt="Logo" className='h-15 auto' />
+          {/* Menu */}
+          <div className="hidden md:flex flex-1 justify-center">
+            <nav
+              aria-label="Global"
+              className="flex items-center gap-6 text-sm"
+            >
+              <Link
+                to="/"
+                className="transition-all duration-150 ease-in-out hover:text-gray-dark hover:text-lg"
+              >
+                Inicio
               </Link>
-            </div>
+              <Link
+                to="/"
+                className="transition-all duration-150 ease-in-out hover:text-gray-dark hover:text-lg"
+              >
+                Sobre la clínica
+              </Link>
+              <Link
+                to="/"
+                className="transition-all duration-150 ease-in-out hover:text-gray-dark hover:text-lg"
+              >
+                Servicio
+              </Link>
+              <Link
+                to="/"
+                className="transition-all duration-150 ease-in-out hover:text-gray-dark hover:text-lg"
+              >
+                Login
+              </Link>
+            </nav>
+          </div>
 
-            {/* Menú */}
-            <div class="hidden md:block pt-10">
-              <nav aria-label="Global" className="flex items-center gap-6 text-sm">
-                  <Link to="/"> Inicio </Link>
-                  <Link to="/"> Sobre la clínica </Link>
-                  <Link to="/"> Servicio </Link>
-              </nav>
-            </div>
+          {/* Hamburguer Menu */}
+          <div className="flex  md:hidden">
 
-            {/* Botones + menú mobile */}
-            <div class="flex items-center gap-4">
-              <div class="sm:flex sm:gap-4">
-                <a
-                  class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm"
-                  href="#"
-                >
-                  Login
-                </a>
-
-                <div class="hidden sm:flex">
-                  <a
-                    class="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
-                    href="#"
-                  >
-                    Register
-                  </a>
-                </div>
-              </div>
-
-              <div class="block md:hidden">
-                <button
-                  class="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="size-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-
+            <button
+              className="text-gray-dark"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {isOpen ? (
+                <i className="fa-solid fa-x text-2xl transition-all duration-150 ease-in-out hover:text-3xl cursor-pointer"></i>
+              ) : (
+                <i className="fa-solid fa-bars text-2xl transition-all duration-150 ease-in-out hover:text-3xl cursor-pointer"></i>
+              )}
+            </button>
           </div>
         </div>
 
-      </header>
-  )
-}
+        {/* Mobile Menu */}
+        {isOpen && (
+          <nav
+            aria-label="Mobile"
+            className="flex flex-col items-center gap-4 pb-4 md:hidden text-sm"
+          >
+            <Link
+              to="/"
+              className="transition-all duration-150 ease-in-out hover:text-gray-dark hover:text-lg"
+              onClick={() => setIsOpen(false)}
+            >
+              Inicio
+            </Link>
+            <Link
+              to="/"
+              className="transition-all duration-150 ease-in-out hover:text-gray-dark hover:text-lg"
+              onClick={() => setIsOpen(false)}
+            >
+              Sobre la clínica
+            </Link>
+            <Link
+              to="/"
+              className="transition-all duration-150 ease-in-out hover:text-gray-dark hover:text-lg"
+              onClick={() => setIsOpen(false)}
+            >
+              Servicio
+            </Link>
+            <Link
+              to="/"
+              className="transition-all duration-150 ease-in-out hover:text-gray-dark hover:text-lg"
+              onClick={() => setIsOpen(false)}
+            >
+              Login
+            </Link>
+          </nav>
+        )}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
