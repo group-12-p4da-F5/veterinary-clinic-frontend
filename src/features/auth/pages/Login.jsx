d66c077fb0a37db9e7f037b8510bfe3b1df20484
Jesus Martin <jesus8.mb@gmail.com>
2025-09-22 15:12:56 +0200

    feat: Input as component

--- src/features/auth/pages/Login.jsx
+++ src/features/auth/pages/Login.jsx
@@ -1,70 +1,75 @@
 import Logo from "../../../shared/assets/Logo.svg";
+import Input from "../components/Input";
 
 const Login = () => {
-  return (
-    <>
-      <div className="flex h-screen">
-        <div className="flex-1 bg-green-light hidden md:flex items-center justify-center">
-          <img src={Logo} alt="Logo" className="max-w-xs" />
-        </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí iría la lógica de login o registro
+    console.log("Formulario enviado");
+  };
 
-        <div className="flex-1 flex items-center justify-center">
+  return (
+    <div className="flex h-screen">
+      {/*  */}
+      <div className="flex-1 bg-green-light hidden md:flex items-center justify-center">
+        <img src={Logo} alt="Logo" className="max-w-xs" />
+      </div>
+
+      {/*  */}
+      <div className="flex-1 flex items-center justify-center">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-sm px-6 flex flex-col"
+        >
           
-          <form className="w-full max-w-sm px-6">
-          <h1 className="text-2xl font-bold mb-8 text-center">Bienvenido</h1>
+          <img src={Logo} alt="Logo" className="max-w-xs hidden-md m-12 md:hidden" />
+
+          <h1 className="text-4xl font-bold mb-8 text-center font-ovo">Bienvenido</h1>
 
           {/* Input DNI */}
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              type="text"
-              name="dni"
-              id="dni"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer"
-              placeholder=" "
+            <Input
+              name = "dni"
+              type = "dni"
+              id = "dni"
+              required = {true}
+              label= "DNI"
             />
-            <label
-              htmlFor="dni"
-              className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              DNI
-            </label>
-          </div>
 
           {/* Input Password */}
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer"
-              placeholder=" "
+            <Input
+              name = "contraseña"
+              type = "contraseña"
+              id = "contraseña"
+              required = {true}
+              label= "Contraseña"
             />
-            <label
-              htmlFor="password"
-              className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Contraseña
-            </label>
-          </div>
 
           {/* Buttons */}
           <div className="flex flex-col gap-3">
-            <button className="w-full py-2 px-4 bg-green-600 text-white rounded-lg hover:bg-green-700">
+            <button
+              type="button"
+              className="w-full py-2 px-4 bg-green-600 text-white rounded-lg hover:bg-green-700 cursor-pointer"
+            >
               Registro
             </button>
-            <button className="w-full py-2 px-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-gray font-bold rounded-lg transition-all duration-150 ease-in-out hover:bg-gray-dark hover:text-white cursor-pointer"
+            >
               Entrar
             </button>
-            <button className=" py-2 px-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600">
+            <button
+              type="button"
+              className="w-full py-2 px-4 bg-gray-500 text-white rounded-lg hover:bg-gray-600 cursor-pointer"
+              onClick={() => window.history.back()}
+            >
               Volver atrás
             </button>
           </div>
         </form>
-
-        </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
 export default Login;
